0773c18a659fe92ac9f621ae8492a10e47f7f821
wisdom6715 <olayiwolaibrahim46@gmail.com>
2024-09-12 14:21:16 +0100

    QR CODE GENERATOR BUILT

---
+++ index.js
@@ -0,0 +1,32 @@
+// /* 
+// 1. Use the inquirer npm package to get user input.
+// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+// 3. Create a txt file to save the user input using the native fs node module.
+// */
+import inquirer from 'inquirer';
+import qr from 'qr-image'
+import fs from 'fs'
+inquirer
+  .prompt([
+    {
+        message: "what's your URL:", 
+        name: "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr-image.pngy'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+ 
+// var qr_svg = qr.image('I love QR!', { type: 'svg' });
+// qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+ 
+// var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
